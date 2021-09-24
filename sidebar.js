d4423044e7bb2428fdc89fa5fd1375ae02da5392
Sundeep Agarwal <learnbyexample.net@gmail.com>
2021-09-24 16:08:39 +0530

    updated book list

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,7 @@
 // Un-active everything when you click it
-Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el, i) {
+Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el) {
     el.addEventHandler("click", function() {
-        Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el, i) {
+        Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el) {
             el.classList.remove("active");
         });
         el.classList.add("active");
@@ -12,17 +12,17 @@ var updateFunction = function() {
 
     var id;
     var elements = document.getElementsByClassName("header");
-    Array.prototype.forEach.call(elements, function(el, i) {
+    Array.prototype.forEach.call(elements, function(el) {
         if (window.pageYOffset >= el.offsetTop) {
             id = el;
         }
     });
 
-    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el, i) {
+    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el) {
         el.classList.remove("active");
     });
 
-    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el, i) {
+    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el) {
         if (id.href.localeCompare(el.href) == 0) {
             el.classList.add("active");
         }
@@ -33,7 +33,7 @@ var updateFunction = function() {
 window.addEventListener('load', function() {
     var pagetoc = document.getElementsByClassName("pagetoc")[0];
     var elements = document.getElementsByClassName("header");
-    Array.prototype.forEach.call(elements, function(el, i) {
+    Array.prototype.forEach.call(elements, function(el) {
         var link = document.createElement("a");
 
         // Indent shows hierarchy
